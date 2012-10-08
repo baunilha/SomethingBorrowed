6d499b50073c97c8373708d822049f3d9ba4431a
baunilha <brucalheiros@gmail.com>
2012-10-08 19:30:22 -0400

    updating to mongodb

--- app.py
+++ app.py
@@ -3,7 +3,28 @@ import os, datetime
 from flask import Flask, request # Retrieve Flask, our framework
 from flask import render_template, url_for
 
+# import all of mongoengine
+# from mongoengine import *
+from flask.ext.mongoengine import mongoengine
+
+# import data models
+import models
+
+import re
+from unidecode import unidecode
+
+connect('mydatabase', host=os.environ.get('MONGOLAB_URI'))
+
 app = Flask(__name__)   # create our flask app
+
+
+# Create the lists that match the name of the ListField in the models.py
+bookType = ['Paperback','Hardcover','PDF','Kindle']
+genre = ['Fiction', 'Programming', 'Physical Computing', 'ITP', 'Design', 'Art']
+itpStatus = ['Current Student', 'ITP Alumni', 'Faculty', 'Resident', 'ITP Adjunct']
+
+
+# --------- Routes ----------
 
 
 # ITP'S CABINET
@@ -75,14 +96,62 @@ def index():
 	return render_template("main.html", books=books)
 
 
+@app.route("/submit", methods=['GET'])
+def submit_form():
+
+	app.logger.debug(request.form.getlist('bookType'))
+	app.logger.debug(request.form.getlist('genre'))
+	app.logger.debug(request.form.getlist('itpStatus'))
+
+	# get new books items form from models.py
+	book_form = models.BookForm(request.form)
+	
+	if request.method == "POST" and book_form.validate():
+	
+		# get form data - create new book
+		book = models.Book()
+		book.title = request.form.get('title','no title')
+		book.slug = slugify(book.title)
+		book.author = request.form.get('author','anonymous')
+		book.description = request.form.get('description','')
+		book.bookType = request.form.getlist('bookType')
+		book.genre = request.form.getlist('genre')
+		book.itpStatus = request.form.getlist('itpStatus')
+		
+		book.save()
+
+		return redirect('/books/%s' % book.slug)
+
 
 # pages inside a category
-@app.route("/books/<title>")
-def show_title(title):
-	# render the template, pass in the animals dictionary refer to it as 'animals'
-	return 'Title %s' % title
+@app.route("/books/<book_slug>")
+def book_display(book_slug):
+	
+	# get book by book_slug
+	try:
+		book = models.Book.objects.get(slug=book_slug)
+	except:
+		abort(404)
+
+	# prepare template data
+	templateData = {
+		'book' : book
+	}
+
+	# render and return the template
+	return render_template('book_entry.html', **templateData)
 
 
+
+# slugify the title 
+# via http://flask.pocoo.org/snippets/5/
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+def slugify(text, delim=u'-'):
+	"""Generates an ASCII-only slug."""
+	result = []
+	for word in _punct_re.split(text.lower()):
+		result.extend(unidecode(word).split())
+	return unicode(delim.join(result))
 
 
 # start the webserver
